bc2dde0f6ce3dd968681e72033cbf25b42645c54
Farrel Belva <farrelirwansyah02@gmail.com>
2024-01-20 01:28:13 +0700

    essssssantaeee

--- resources/views/perma2020.blade.php
+++ resources/views/perma2020.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.5.0/remixicon.min.css" />
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="styleperma2020.css" />
     <title>PERMA 2020</title>
   </head>
 
