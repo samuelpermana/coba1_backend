428eba6d6695980e16ae75c50eb6d6cd4da93707
WilliamSnjy <william.sanjaya50@gmail.com>
2024-01-20 01:22:52 +0700

    kelarnihbos

--- public/script2.js
+++ public/script2.js
@@ -1,3 +1,18 @@
+var acc = document.getElementsByClassName("accordion-k");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+        } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+    }
+
 let swiperCards = new Swiper(".card__content", {
     loop: true,
     spaceBetween: 32,
