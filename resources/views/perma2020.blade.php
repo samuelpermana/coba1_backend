f684edb1cd718e6696058d0a5ba9b4e359bf338c
Farrel Belva <farrelirwansyah02@gmail.com>
2024-01-19 12:52:22 +0700

    kerjaanfarrel

---
+++ resources/views/perma2020.blade.php
@@ -0,0 +1,146 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.5.0/remixicon.min.css" />
+    <link rel="stylesheet" href="style.css" />
+    <title>PERMA 2020</title>
+  </head>
+
+  <body>
+    <nav>
+      <div class="nav-logo">
+        <a href="#">
+          <img src="/img/coba1.png" alt="Logo" />
+        </a>
+      </div>
+
+      <ul class="nav-links">
+        <li class="link"><a href="index.html">Home</a></li>
+        <li id="link1" class="link"><a href="kotakaspirasi.html">Kotak Aspirasi</a></li>
+        <li id="link2" class="link"><a href="faq.html">FAQ</a></li>
+        <li id="link3" class="link"><a href="bankaspirasi.html">Bank Aspirasi</a></li>
+        <li id="link4" class="link"><a href="selayangpandang.html">Selayang Pandang</a></li>
+        <li id="link4" class="link"><a href="jdih.html">JDIH</a></li>
+        <li id="link6" class="link"><a href="peminjamanruangan.html">Peminjaman Ruangan</a></li>
+      </ul>
+      <button class="btn">Ajukan Surat</button>
+    </nav>
+
+    <div class="pricing-JD">
+        <div class="card-JD1">
+          <div class="content">
+            <table class="content-table">
+                <thead>
+                  
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Tipe Dokumen</td>
+                    <td>Monografi Hukum</td>
+                    
+                    
+                  </tr>
+                  <tr class="active-row">
+                    <td>Judul</td>
+                    <td>Hukum Keuangan Negara</td>
+                    
+                    
+                  </tr>
+                  <tr>
+                    <td>T.E.U.</td>
+                    <td>Burhanuddin, nizam</td>
+                    
+                    
+                  </tr>
+                  <tr>
+                    <td>Nomor Panggil</td>
+                    <td>343.03 BUR h</td>
+                    
+                    
+                  </tr>
+                  <tr>
+                    <td>Tempat Terbit</td>
+                    <td>Yogyakarta</td>
+                    
+                    
+                  </tr>
+                  <tr>
+                    <td>Indonesia</td>
+                    <td>slobehh
+                    </td>
+                    
+                    
+                  </tr>
+                  <tr>
+                    <td>Penerbit</td>
+                    <td>Total Media</td>
+                    
+                    
+                  </tr>
+                  <tr>
+                    <td>ISBN/ISSN</td>
+                    <td>978-602-19601-2-0</td>
+                    
+                    
+                  </tr>
+                  <tr>
+                    <td>Bahasa</td>
+                    <td>Indonesia</td>
+                    
+                    
+                  </tr>
+                </tbody>
+              </table>
+          </div>
+        </div>
+        
+        
+      </div>
+
+    
+    
+    
+   
+
+    <footer class="container">
+      <span class="blur"></span>
+      <span class="blur"></span>
+      <div class="column">
+        <div class="logo">
+          <img src="/img/coba1.png" />
+        </div>
+        <p>SENAT MAHASISWA FAKULTAS HUKUM UNDIP</p>
+        <div class="socials">
+          <a href="#"><i class="ri-youtube-line"></i></a>
+          <a href="#"><i class="ri-instagram-line"></i></a>
+          <a href="#"><i class="ri-twitter-line"></i></a>
+        </div>
+      </div>
+      <div class="column">
+        <h4>CONTOH</h4>
+        <a href="#">Template</a>
+        <a href="#">Template</a>
+        <a href="#">Template</a>
+      </div>
+      <div class="column">
+        <h4>About Us</h4>
+        <a href="#">Blogs</a>
+        <a href="#">Channels</a>
+        <a href="#">Sponsors</a>
+      </div>
+      <div class="column">
+        <h4>Contact</h4>
+        <a href="#">Contact Us</a>
+        <a href="#">Privicy Policy</a>
+        <a href="#">Terms & Conditions</a>
+      </div>
+    </footer>
+
+    <div class="copyright">Copyright Â© 2023 SENAT FH UNDIP. All Rights Reserved.</div>
+
+    <script src="script.js"></script>
+  </body>
+</html>
