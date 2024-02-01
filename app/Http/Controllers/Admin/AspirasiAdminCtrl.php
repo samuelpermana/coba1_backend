a42f50c4ef01e9087a2596a5c2c39f7bdb3c897d
samuelpermana <permana20samuel@gmail.com>
2024-02-01 14:13:35 +0700

    bug fixing

--- app/Http/Controllers/Admin/AspirasiAdminCtrl.php
+++ app/Http/Controllers/Admin/AspirasiAdminCtrl.php
@@ -8,7 +8,7 @@ use App\Models\Aspirasi;
 
 class AspirasiAdminCtrl extends Controller
 {
-    public function get()
+    public function index()
     {
         $aspirasis = Aspirasi::all();
         return view('cms.aspirasi', compact('aspirasis'));
